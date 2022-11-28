8c1f3891768af239e993c60b95cec278cc7f4550
Kasuni <kasunirashmika568@gmail.com>
2022-11-28 16:07:13 +0530

    Added changes to MSTest

--- Task_2/MSTest.cs
+++ Task_2/MSTest.cs
@@ -1,4 +1,6 @@
 using MSTest;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Task_2
 {
@@ -6,6 +8,8 @@ namespace Task_2
     [TestClass]
     public class MSTest
     {
+        IWebDriver driver;
+
         public TestContext TestContext { get; set; }
         static int testcounter = 0;
 
@@ -18,6 +22,8 @@ namespace Task_2
         [TestInitialize]
         public void TestSetup()
         {
+            driver = new ChromeDriver("C:\\Users\\UPRABKA\\Documents\\C# traning\\5 - Selenium Web Driver\\chromedriver_win32\\");
+            driver.Manage().Window.Maximize();
             testcounter++;
             Console.WriteLine("Test started number {0}", testcounter);
             TestContext.WriteLine("MSTest Test Setup output");
@@ -26,14 +32,12 @@ namespace Task_2
         [TestMethod]
         public void TestMethod1()
         {
-            int arg1 = 10;
-            int arg2 = 10;
-            double expected = 0;
-
-            double result = Calculator.Sub(arg1, arg2);
-            Assert.AreEqual(expected, result);
-
-            TestContext.WriteLine("expected and actual values are equal");
+            driver.Url = "http://www.google.com";
+            string test = "Geeks";
+            Assert.AreEqual("Geeks", test);
+            driver.FindElement(By.XPath("//input[@title='Search']")).SendKeys(test);
+            driver.SwitchTo().ActiveElement().SendKeys(Keys.Enter);
+            TestContext.WriteLine("Input text Geeks");
         }
 
         [Ignore("MSTest ignore")]
@@ -47,20 +51,18 @@ namespace Task_2
         [TestMethod]
         public void TestMethod3()
         {
-            int arg1 = 10;
-            int arg2 = 10;
-            double expected = 21;
-            
-            double result = Calculator.Add(arg1, arg2);
-            Assert.AreEqual(expected, result);
-
-            TestContext.WriteLine("expected and actual values are not equal");
+            driver.Url = "http://www.google.com";
+            string test = "Geeks";
+            Assert.AreEqual("Geek", test);
+            driver.FindElement(By.XPath("//input[@title='Search']")).SendKeys(test);
+            driver.SwitchTo().ActiveElement().SendKeys(Keys.Enter);
         }
 
 
         [TestCleanup]
         public void MSTestCleanup()
         {
+            driver.Dispose();
             Console.WriteLine("Test number {0} is compleated", testcounter);
             TestContext.WriteLine("MSTest Test Cleanup output");
         }
